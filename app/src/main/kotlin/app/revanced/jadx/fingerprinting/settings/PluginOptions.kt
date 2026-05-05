cf9dad2182e50146c03bf9d02672338021dce275
Kofhisho <contact@alphexo.dev>
2026-05-05 23:34:28 +0100

    feat(Options): Added missing switches

--- app/src/main/kotlin/app/revanced/jadx/fingerprinting/settings/PluginOptions.kt
+++ app/src/main/kotlin/app/revanced/jadx/fingerprinting/settings/PluginOptions.kt
@@ -10,6 +10,14 @@ class PluginOptions : BasePluginOptionsBuilder() {
         private set
     var solverUseOpcodes: Boolean = false
         private set
+    var solverUseReturnType: Boolean = true
+        private set
+    var solverUseParameters: Boolean = true
+        private set
+    var solverUseStrings: Boolean = true
+        private set
+    var solverUseAccessFlags: Boolean = true
+        private set
     var solverUseDefiningClass: Boolean = true
         private set
     var solverUseMethodName: Boolean = true
@@ -30,6 +38,30 @@ class PluginOptions : BasePluginOptionsBuilder() {
             .setter { v -> solverUseOpcodes = v }
             .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
 
+        boolOption("$id.solver.use-return-type")
+            .description("Include return type as a solver feature")
+            .defaultValue(true)
+            .setter { v -> solverUseReturnType = v }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.solver.use-parameters")
+            .description("Include parameter types as solver features")
+            .defaultValue(true)
+            .setter { v -> solverUseParameters = v }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.solver.use-strings")
+            .description("Include string constants as solver features")
+            .defaultValue(true)
+            .setter { v -> solverUseStrings = v }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.solver.use-access-flags")
+            .description("Include access flags as a solver feature")
+            .defaultValue(true)
+            .setter { v -> solverUseAccessFlags = v }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
         boolOption("$id.solver.use-defining-class")
             .description("Include defining class as a solver feature (disable for heavily obfuscated APKs)")
             .defaultValue(true)
@@ -45,6 +77,10 @@ class PluginOptions : BasePluginOptionsBuilder() {
 
     fun toSolverSettings(): SolverSettings = SolverSettings(
         useOpcodes = solverUseOpcodes,
+        useReturnType = solverUseReturnType,
+        useParameters = solverUseParameters,
+        useStrings = solverUseStrings,
+        useAccessFlags = solverUseAccessFlags,
         useDefiningClass = solverUseDefiningClass,
         useMethodName = solverUseMethodName,
     )
