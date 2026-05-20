ab173d766c97a11f8a441e9427e36c6a31d26714
Kofhisho <contact@alphexo.dev>
2026-05-20 23:01:43 +0100

    refactor(settings): Save code panel defaults

--- app/src/main/kotlin/app/revanced/jadx/fingerprinting/settings/PluginOptions.kt
+++ app/src/main/kotlin/app/revanced/jadx/fingerprinting/settings/PluginOptions.kt
@@ -2,6 +2,8 @@ package app.revanced.jadx.fingerprinting.settings
 
 import app.revanced.jadx.fingerprinting.ReVancedJadxPlugin
 import app.revanced.jadx.fingerprinting.solver.SolverSettings
+import app.revanced.jadx.fingerprinting.ui.components.CodePanel
+import app.revanced.jadx.fingerprinting.ui.components.EditorSettingsHolder
 import jadx.api.plugins.options.OptionFlag
 import jadx.api.plugins.options.impl.BasePluginOptionsBuilder
 
@@ -21,6 +23,8 @@ class PluginOptions : BasePluginOptionsBuilder() {
     var solverUseDefiningClass: Boolean = true
         private set
     var solverUseMethodName: Boolean = true
+        private set
+    var editorSettings: CodePanel.Settings = CodePanel.Settings()
         private set
 
     override fun registerOptions() {
@@ -73,6 +77,60 @@ class PluginOptions : BasePluginOptionsBuilder() {
             .defaultValue(true)
             .setter { v -> solverUseMethodName = v }
             .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.editor.enable-linting")
+            .description("Live Kotlin lint in the fingerprint eval editor")
+            .defaultValue(EDITOR_DEFAULTS.enableLinting)
+            .setter { v -> updateEditor { copy(enableLinting = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        intOption("$id.editor.lint-debounce-ms")
+            .description("Milliseconds of editor inactivity before the linter re-compiles")
+            .defaultValue(EDITOR_DEFAULTS.lintDebounceMs)
+            .setter { v -> updateEditor { copy(lintDebounceMs = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.editor.autocomplete-enabled")
+            .description("Auto-show autocomplete popup as you type (vs. only on Ctrl+Space)")
+            .defaultValue(EDITOR_DEFAULTS.autoCompleteEnabled)
+            .setter { v -> updateEditor { copy(autoCompleteEnabled = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        intOption("$id.editor.autocomplete-delay-ms")
+            .description("Milliseconds of inactivity before the autocomplete popup appears")
+            .defaultValue(EDITOR_DEFAULTS.autoCompleteDelayMs)
+            .setter { v -> updateEditor { copy(autoCompleteDelayMs = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.editor.parameter-assistance")
+            .description("Show parameter hint popups after `(`")
+            .defaultValue(EDITOR_DEFAULTS.parameterAssistance)
+            .setter { v -> updateEditor { copy(parameterAssistance = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.editor.paint-tab-lines")
+            .description("Paint vertical tab/indent guide lines")
+            .defaultValue(EDITOR_DEFAULTS.paintTabLines)
+            .setter { v -> updateEditor { copy(paintTabLines = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.editor.anti-aliasing")
+            .description("Anti-aliasing editor text")
+            .defaultValue(EDITOR_DEFAULTS.antiAliasing)
+            .setter { v -> updateEditor { copy(antiAliasing = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.editor.auto-indent")
+            .description("Auto-indent new lines based on the previous line")
+            .defaultValue(EDITOR_DEFAULTS.enableAutoIndent)
+            .setter { v -> updateEditor { copy(enableAutoIndent = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
+
+        boolOption("$id.editor.show-whitespace")
+            .description("Render whitespace characters (spaces, tabs) as visible markers")
+            .defaultValue(EDITOR_DEFAULTS.whitespaceCharacters)
+            .setter { v -> updateEditor { copy(whitespaceCharacters = v) } }
+            .flags(OptionFlag.PER_PROJECT, OptionFlag.NOT_CHANGING_CODE)
     }
 
     fun toSolverSettings(): SolverSettings = SolverSettings(
@@ -84,4 +142,13 @@ class PluginOptions : BasePluginOptionsBuilder() {
         useDefiningClass = solverUseDefiningClass,
         useMethodName = solverUseMethodName,
     )
+
+    private inline fun updateEditor(transform: CodePanel.Settings.() -> CodePanel.Settings) {
+        editorSettings = editorSettings.transform()
+        EditorSettingsHolder.update(editorSettings)
+    }
+
+    private companion object {
+        val EDITOR_DEFAULTS = CodePanel.Settings()
+    }
 }
